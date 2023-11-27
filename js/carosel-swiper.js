da7bb88e5150a12964b0b5e39fad5f69dca1bf27
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-11-27 23:56:41 +0500

    Broker carosel section commit

--- js/carosel-swiper.js
+++ js/carosel-swiper.js
@@ -27,69 +27,26 @@ var swiper = new Swiper("#brokers-carosel", {
 		// disableOnInteraction: true,
 	},
 	breakpoints: {
-		414: {
+		325: {
 			slidesPerView: 1,
 			spaceBetween: 0,
 		},
-		540: {
-			slidesPerView: 2,
-			spaceBetween: 0,
-		},
-		620: {
-			slidesPerView: 3,
-			spaceBetween: 5,
-		},
-		991: {
-			slidesPerView: 3,
-			spaceBetween: 5,
-		},
-		1024: {
-			slidesPerView: 5,
-			spaceBetween: 5,
-		},
-	},
-	grabCursor: true,
-});
-
-//agents-swiper
-var swiper2 = new Swiper("#agents-swiper", {
-	slidesPerView: 1,
-	slidesPerGroup: 4,
-	spaceBetween: 30,
-	grabCursor: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	autoplay: {
-		delay: 4000,
-	},
-	breakpoints: {
 		414: {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			slidesPerGroup: 1,
-		},
-		540: {
 			slidesPerView: 2,
 			spaceBetween: 10,
-			slidesPerGroup: 2,
 		},
 		620: {
 			slidesPerView: 3,
 			spaceBetween: 15,
-			slidesPerGroup: 3,
 		},
-		991: {
-			slidesPerView: 3,
-			spaceBetween: 20,
-			slidesPerGroup: 3,
-		},
-		1024: {
+		992: {
 			slidesPerView: 4,
-			slidesPerGroup: 4,
+			spaceBetween: 20,
+		},
+		1025: {
+			slidesPerView: 5,
 			spaceBetween: 20,
 		},
 	},
-	loop: true,
+	grabCursor: true,
 });

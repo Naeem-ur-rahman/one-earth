02cc42529bed168ee91c86894f0d60fa0e52bc6a
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-11-30 19:48:26 +0500

    Navbar scripting + deisgning

--- js/script.js
+++ js/script.js
@@ -11,6 +11,35 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Navbar Handle function
+    let navbar_down = document.querySelectorAll('.navbar-item.down');
+    const navbarHandle = () => {
+        navbar_down.forEach(nav_down => {
+            const link = nav_down.querySelector("a")
+            const link_href = link.href
+            if (window.matchMedia('(max-width: 991px)').matches) {
+                link.href = 'javascript:void'
+            } else {
+                link.href = link_href;
+            }
+        });
+    }
+    // Navbar click mobile screen
+    window.addEventListener('load', navbarHandle())
+    window.addEventListener('resize', navbarHandle())
+    screen.addEventListener('load', navbarHandle())
+    screen.addEventListener('resize', navbarHandle())
+
+    // Navbar links click and trigger the nav-dropdown
+    for (let i = 0; i < navbar_down.length; i++) {
+        const nav_dropdown = navbar_down[i].querySelector('.nav-dropdown')
+        const clickBtn = navbar_down[i].querySelector('a');
+        clickBtn.addEventListener('click', () => {
+            nav_dropdown.classList.toggle('active');
+        })
+    }
+
+
     let navbar = document.querySelector('.navbar')
     let menu = document.querySelector('#hamburg i')
 

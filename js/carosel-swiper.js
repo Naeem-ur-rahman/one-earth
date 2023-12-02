cff776eab1b2b42a14111268f8b9e85a181a4ed4
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-12-02 17:02:33 +0500

    Property detail page similar products section

--- js/carosel-swiper.js
+++ js/carosel-swiper.js
@@ -80,6 +80,37 @@ var swiper = new Swiper("#clients-carosel", {
 	loop: true,
 });
 
+var swiper = new Swiper("#properties-carosel", {
+	slidesPerView: 1,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	autoplay: {
+		delay: 4000,
+		// disableOnInteraction: true,
+	},
+	breakpoints: {
+		414: {
+			slidesPerView: 1,
+			spaceBetween: 5,
+		},
+		481: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+		},
+		766: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1200: {
+			slidesPerView: 4,
+			spaceBetween: 20,
+		},
+	},
+	grabCursor: true,
+});
+
 //partners-swiper
 var swiper3 = new Swiper("#partners-swiper", {
 	slidesPerView: 2,
